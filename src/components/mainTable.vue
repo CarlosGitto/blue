c8fe796867a21a07e37e9050cc07a8aea968416c
Carlos Gitto <carlosgitto98@gmail.com>
2022-11-02 09:33:40 -0300

    Add new Tasks

---
+++ src/components/mainTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- This component should only be the list with the lines of code shown on the blue simulator  -->
+  <!-- Must be done with v-for, decToOct, parseOct, must be rendered dynamicly -->
+  <!-- You can set a constant with the number 4096 to loop from 0 to 7777 on octal -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "mainTable",
+};
+</script>
+
+<style></style>
---
+++ src/js/conversion.js
@@ -0,0 +1,24 @@
+/* In this file you can find the way to go from one data type to another */
+
+export function hexToDec() {
+  // Use hexToBin then binToDec then return
+}
+export function octToDec() {
+  // Equal to hexToDec
+}
+export function binToDec() {}
+
+export function decToBin() {}
+
+export function octToBin() {}
+
+export function hexToBin() {}
+
+export function parseBin() {
+  "1";
+  "0000 0000 0000 0001";
+}
+
+export function parseOct() {}
+
+export function parseHex() {}
